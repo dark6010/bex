f7b5cbbe869bb52f33e7ec6b69f721123e4e8124
grover plaza quiroga <grover.plaza@gmail.com>
2017-03-14 13:45:20 -0400

    sss

---
+++ 04/buffer.js
@@ -0,0 +1,18 @@
+'use strict'
+var buff=new Buffer(6),
+    buff2=new Buffer(26),
+    str='\u00bd + \u00bc = \u00be'
+buff.write('abcd', 0, 'ascii')
+console.log(
+    buff,
+    buff.toString('ascii'),
+    str,
+    str.length,
+    Buffer.byteLength(str, 'utf8'),
+    buff2.length
+)
+
+for (var i= 0;i<buff2.length;i++){
+    buff2[i] = i+97
+}
+console.log(buff2.toString('ascii'))
